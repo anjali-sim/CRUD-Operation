8c41eb358ba912459e6a2be4fdcf9ea51e4cea2d
anjali-sim <anjalighetia123@gmail.com>
2023-03-21 11:04:54 +0530

    :hammer: Updated the image validation condition

--- src/script/script.js
+++ src/script/script.js
@@ -1,13 +1,34 @@
 /**
- * @function validateForm
- * @description to validate the form inputs before submitting the data
- * @params void
- * @returns boolean
- * Examples :
- * - If any of the field is not entered, then an alert box pops up with the message that the particular field is required.
- * - The price field can only contain numeric values, not less than one(negative numbers) and cannot start with zero(0).
+ * @function convertBase64
+ * @description
+ * @param {*} file
+ * @returns promise object
  */
-function validateForm() {
+const convertBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+  
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+  
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  };
+  
+  /**
+   * @function validateForm
+   * @description to validate the form inputs before submitting the data
+   * @params void
+   * @returns boolean
+   * Examples :
+   * - If any of the field is not entered, then an alert box pops up with the message that the particular field is required.
+   * - The price field can only contain numeric values, not less than one(negative numbers) and cannot start with zero(0).
+   */
+  function validateForm() {
     let id = document.getElementById("id").value;
     let name = document.getElementById("name").value;
     let image = document.getElementById("image").value;
@@ -64,30 +85,47 @@ function validateForm() {
       document.getElementById("imageWarning").style.color = "red";
       return false;
     }
+    const extension = image.split(".").pop().toLowerCase();
   
-    const extension = image.split('.').pop().toLowerCase();
-    
-      // Check if the file extension is valid
-      if (!(extension === "jpg" || extension === "jpeg" || extension === "png" || extension === "gif")) {
-          document.getElementById("imageWarning").innerText = "Image must be of valid extension.";
-          document.getElementById("imageWarning").style.color = "red";
-          return false;
-      }
-    
+    // Check if the file extension is valid
+    if (
+      !(
+        extension === "jpg" ||
+        extension === "jpeg" ||
+        extension === "png" ||
+        extension === "gif" 
+      )
+    ) {
+      document.getElementById("imageWarning").innerText =
+        "Image must be of valid extension.";
+      document.getElementById("imageWarning").style.color = "red";
+      return false;
+    }
+  
+    const fileInput = document.getElementById("image");
+    const file = fileInput.files[0];
+    const fileSizeKB = file.size / 1024;
+    if(fileSizeKB > 2048){
+      document.getElementById("imageWarning").innerText =
+      "Image Size must be of less than 1 Mb.";
+    document.getElementById("imageWarning").style.color = "red";
+    return false;
+    }
+  
     imageWarning.innerHTML =
       '<i class="fa-sharp fa-regular fa-circle-check"></i>';
     return true;
   }
-
+  
   /**
- * @function displayData
- * @description to display the data in the table format
- * @params void
- * @returns void
- * Examples :
- * - If the user adds the data and submits the form, then that data is displayed in the table
- */
-function displayData() {
+   * @function displayData
+   * @description to display the data in the table format
+   * @params void
+   * @returns void
+   * Examples :
+   * - If the user adds the data and submits the form, then that data is displayed in the table
+   */
+  function displayData() {
     let productList;
     if (localStorage.getItem("productList") == null) {
       productList = [];
@@ -97,22 +135,22 @@ function displayData() {
       let result = "";
   
       productList.forEach(function (element, index) {
-          result += "<tr>";
-          result += "<td>" + element.id + "</td>";
-          result += "<td>" + element.name + "</td>";
-          result +=
-          `<td> <img src=` +
+        result += "<tr>";
+        result += "<td>" + element.id + "</td>";
+        result += "<td>" + element.name + "</td>";
+        result +=
+          `<td> <img class = "rounded mx-auto d-block" src=` +
           element.image +
-          ` style="width:100%;height:98%"> </td>`;
-          result += "<td>" + element.price + "</td>";
-          result += "<td>" + element.description + "</td>";
-          result +=
+          ` style="width:80%;height:70%;"> </td>`;
+        result += "<td>" + element.price + "</td>";
+        result += "<td>" + element.description + "</td>";
+        result +=
           '<td><button onclick="updateData(' +
           index +
           ')" class="btn btn-warning mb-2">Edit</button>&emsp;<button onclick="deleteData(' +
           index +
           ')" class="btn btn-danger mb-2">Delete</button></td>';
-          result += "</tr>";
+        result += "</tr>";
       });
   
       document.querySelector("#crudTable tbody").innerHTML = result;
@@ -134,7 +172,9 @@ function displayData() {
     if (validateForm() == true) {
       const id = Math.floor(Math.random() * 1000);
       const name = document.getElementById("name").value;
-      const image = await convertBase64(document.getElementById("image").files[0]);
+      const image = await convertBase64(
+        document.getElementById("image").files[0]
+      );
       const price = document.getElementById("price").value;
       const description = document.getElementById("description").value;
   
@@ -162,13 +202,13 @@ function displayData() {
     }
   }
   
-/**
- * @function deleteData
- * @description to delete the data from the local storage
- * @param {*} index
- * @returns void
- */
-function deleteData(index) {
+  /**
+   * @function deleteData
+   * @description to delete the data from the local storage
+   * @param {*} index
+   * @returns void
+   */
+  function deleteData(index) {
     let productList;
     if (localStorage.getItem("productList") == null) {
       productList = [];
@@ -180,16 +220,16 @@ function deleteData(index) {
     localStorage.setItem("productList", JSON.stringify(productList));
     displayData();
   }
-
+  
   /**
- * @function updateData
- * @description to update the data from the local storage
- * @param {*} index
- * @returns void
- * Examples :
- * - If the user has changed any particular input field, then it will be updated and stored in the local storage and will be displayed in the table
- */
-async function updateData(index) {
+   * @function updateData
+   * @description to update the data from the local storage
+   * @param {*} index
+   * @returns void
+   * Examples :
+   * - If the user has changed any particular input field, then it will be updated and stored in the local storage and will be displayed in the table
+   */
+  async function updateData(index) {
     document.getElementById("submit").style.display = "none";
     document.getElementById("update").style.display = "block";
     var productList;
@@ -231,14 +271,14 @@ async function updateData(index) {
   }
   
   /**
- * @function displaySearchedData
- * @description to display only the searched data in the table
- * @param {*} products
- * @returns void
- * Examples :
- * - If the user entered a particular product name, then only that data will be displayed in the table
- */
-function displaySearchedData(products) {
+   * @function displaySearchedData
+   * @description to display only the searched data in the table
+   * @param {*} products
+   * @returns void
+   * Examples :
+   * - If the user entered a particular product name, then only that data will be displayed in the table
+   */
+  function displaySearchedData(products) {
     let obj = products;
   
     var result = "";
@@ -289,14 +329,14 @@ function displaySearchedData(products) {
     console.log(sortedItem);
     displaySearchedData(sortedItem);
   }
-
+  
   /**
- * @function sortItems
- * @description
- * @params
- * @returns
- */
-function sortItems(sortvalue) {
+   * @function sortItems
+   * @description
+   * @params
+   * @returns
+   */
+  function sortItems(sortvalue) {
     let products = JSON.parse(localStorage.getItem("productList")) ?? [];
     console.log(products);
     if (sortvalue === "productid") {
@@ -310,3 +350,4 @@ function sortItems(sortvalue) {
     console.log(products);
     displayData();
   }
+  
